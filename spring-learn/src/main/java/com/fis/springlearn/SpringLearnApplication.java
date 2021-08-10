d933706050c5c71c3d107557927bacfd3a50c786
Rutuja <patilrutuja436@gmail.com>
2021-08-10 11:45:39 +0530

    Added my Country bean project

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -3,29 +3,44 @@ package com.fis.springlearn;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		displayDate();
+//		SpringApplication.run(SpringLearnApplication.class, args);
+
+		// displayDate();
+		displayCountry();
 	}
-	static void displayDate()  {
+
+	static void displayCountry() {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+		context.close();
+	}
+
+	static void displayDate() {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-		 //Date parseDate;
-		 try {
-		Date parseDate = format.parse("31/12/2018");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		// Date parseDate;
+		try {
+			Date parseDate = format.parse("31/12/2018");
 			System.out.println(parseDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+		context.close();
 	}
 
 }
