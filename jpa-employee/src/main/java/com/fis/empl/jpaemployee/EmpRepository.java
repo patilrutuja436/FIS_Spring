3fdeda66d477bad225156c3d2f1585d6d4fcad02
Rutuja <patilrutuja436@gmail.com>
2021-08-12 12:13:32 +0530

    Postman CRUD in springjpa-User

---
+++ jpa-employee/src/main/java/com/fis/empl/jpaemployee/EmpRepository.java
@@ -0,0 +1,8 @@
+package com.fis.empl.jpaemployee;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface EmpRepository extends CrudRepository<Employee, Integer>{
+
+}
---
+++ jpa-employee/src/test/java/com/fis/empl/jpaemployee/JpaEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.empl.jpaemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
